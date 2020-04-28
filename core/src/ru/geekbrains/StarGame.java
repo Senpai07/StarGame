145e3a7128eaca1defaaa39c2a4d64a90a3f2217
Senpai <senpai@mail.ru>
2020-04-29 00:06:30 +0300

    Lesson2 homework.

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -1,44 +1,13 @@
 package ru.geekbrains;
 
-import com.badlogic.gdx.Application;
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class StarGame extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture background;
-    private Sprite backgroundSprite;
+import ru.geekbrains.screens.MenuScreen;
+
+public class StarGame extends Game {
 
     @Override
     public void create() {
-        if (Gdx.app.getType().equals(Application.ApplicationType.Android)) {
-            Gdx.graphics.setWindowedMode(1080, 1920);
-            background = new Texture("background_v.jpg");
-        } else {
-            Gdx.graphics.setWindowedMode(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-            background = new Texture("background.jpg");
-        }
-        batch = new SpriteBatch();
-        backgroundSprite = new Sprite(background);
-        backgroundSprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-    }
-
-    @Override
-    public void render() {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        backgroundSprite.draw(batch);
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        background.dispose();
+        setScreen(new MenuScreen());
     }
 }
---
+++ core/src/ru/geekbrains/screens/MenuScreen.java
@@ -0,0 +1,100 @@
+package ru.geekbrains.screens;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.base.BaseScreen;
+
+public class MenuScreen extends BaseScreen {
+    private static final int SPEED = 10;
+    private Texture background;
+    private Texture spaceShip;
+    private Sprite backgroundSprite;
+    private Sprite spaceShipSprite;
+    private Vector2 positionSpaceShipVector;
+    private Vector2 speedSpaceShipVector;
+    private Vector2 targetVector;
+    private Vector2 directionVector;
+    private Vector2 distanceVector;
+
+    @Override
+    public void show() {
+        super.show();
+        if (Gdx.app.getType().equals(Application.ApplicationType.Android)) {
+            Gdx.graphics.setWindowedMode(1080, 1920);
+            background = new Texture("StarsSky_v1.jpg");
+        } else {
+            Gdx.graphics.setWindowedMode(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+            background = new Texture("StarsSky1.jpg");
+        }
+        backgroundSprite = new Sprite(background);
+        backgroundSprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        spaceShip = new Texture("SpaceShip1.png");
+        spaceShipSprite = new Sprite(spaceShip);
+        spaceShipSprite.setPosition(0, 0);
+        spaceShipSprite.setSize(200, 200);
+
+        positionSpaceShipVector = new Vector2();
+        speedSpaceShipVector = new Vector2(SPEED, SPEED);
+        targetVector = new Vector2();
+        directionVector = new Vector2();
+        distanceVector = new Vector2();
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        if (distanceVector.len() > SPEED - 1) {
+            directionVector.set(distanceVector);
+            directionVector.nor();
+            if (distanceVector.len() < SPEED) {
+                positionSpaceShipVector.set(targetVector);
+            } else {
+                directionVector.scl(speedSpaceShipVector);
+                positionSpaceShipVector.add(directionVector);
+            }
+            spaceShipSprite.setPosition(positionSpaceShipVector.x, positionSpaceShipVector.y);
+            distanceVector = targetVector.cpy().sub(positionSpaceShipVector);
+//            System.out.printf("positionSpaceShipVector (%f, %f)%n", positionSpaceShipVector.x, positionSpaceShipVector.y);
+//            System.out.printf("targetVector (%f, %f)%n", targetVector.x, targetVector.y);
+//            System.out.printf("distanceVector (%f, %f)%n", distanceVector.x, distanceVector.y);
+//            System.out.println("distanceVector.len() = " + distanceVector.len());
+        }
+        batch.begin();
+        backgroundSprite.draw(batch);
+        spaceShipSprite.draw(batch);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        background.dispose();
+        super.dispose();
+    }
+
+    private void StartMove(int screenX, int screenY) {
+        targetVector.set(screenX - spaceShipSprite.getHeight() / 2,
+                Gdx.graphics.getHeight() - screenY - spaceShipSprite.getHeight() / 2);
+        System.out.printf("Координаты targetVector (%f, %f)%n", targetVector.x, targetVector.y);
+        distanceVector = targetVector.cpy().sub(positionSpaceShipVector);
+        System.out.printf("Координаты distanceVector (%f, %f)%n", distanceVector.x, distanceVector.y);
+
+        System.out.println("distanceVector.len() = " + distanceVector.len());
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        StartMove(screenX, screenY);
+        return super.touchDown(screenX, screenY, pointer, button);
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        StartMove(screenX, screenY);
+        return super.touchDragged(screenX, screenY, pointer);
+    }
+}
