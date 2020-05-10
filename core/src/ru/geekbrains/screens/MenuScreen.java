acdff4c91f55847769847529e5096b44f1b70bf3
Senpai <senpai@mail.ru>
2020-05-11 00:06:27 +0300

    Lesson3 homework. Main.

--- core/src/ru/geekbrains/screens/MenuScreen.java
+++ core/src/ru/geekbrains/screens/MenuScreen.java
@@ -1,95 +1,65 @@
 package ru.geekbrains.screens;
 
-import com.badlogic.gdx.Application;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.BaseScreen;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.sprites.Background;
+import ru.geekbrains.sprites.Logo;
 
 public class MenuScreen extends BaseScreen {
-    private static final int SPEED = 7;
     private Texture background;
-    private Texture spaceShip;
-    private Sprite backgroundSprite;
-    private Sprite spaceShipSprite;
-    private Vector2 positionSpaceShipVector;
-    private Vector2 targetVector;
-    private Vector2 directionVector;
-    private Vector2 distanceVector;
+    private Background backgroundSprite;
+    private Texture logo;
+    private Logo logoSprite;
 
     @Override
     public void show() {
         super.show();
-        if (Gdx.app.getType().equals(Application.ApplicationType.Android)) {
-            Gdx.graphics.setWindowedMode(1080, 1920);
-            background = new Texture("StarsSky_v1.jpg");
-        } else {
-            Gdx.graphics.setWindowedMode(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-            background = new Texture("StarsSky1.jpg");
-        }
-        backgroundSprite = new Sprite(background);
-        backgroundSprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-        spaceShip = new Texture("SpaceShip1.png");
-        spaceShipSprite = new Sprite(spaceShip);
-        spaceShipSprite.setPosition(0, 0);
-        spaceShipSprite.setSize(200, 200);
+        background = new Texture("StarsSky_v1.jpg");
+        backgroundSprite = new Background(background);
 
-        positionSpaceShipVector = new Vector2();
-        targetVector = new Vector2();
-        directionVector = new Vector2();
-        distanceVector = new Vector2();
+        logo = new Texture("SpaceShip1.png");
+        logoSprite = new Logo(logo);
+        logoSprite.pos.set(0, 0);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        distanceVector.set(targetVector);
-        if (distanceVector.sub(positionSpaceShipVector).len() > SPEED) {
-            positionSpaceShipVector.add(directionVector);
-        } else {
-            positionSpaceShipVector.set(targetVector);
-            directionVector.setZero();
-        }
-        spaceShipSprite.setPosition(positionSpaceShipVector.x, positionSpaceShipVector.y);
+        logoSprite.update(delta);
         batch.begin();
         backgroundSprite.draw(batch);
-        spaceShipSprite.draw(batch);
+        logoSprite.draw(batch);
         batch.end();
     }
 
     @Override
     public void dispose() {
         background.dispose();
+        logo.dispose();
         super.dispose();
-    }
-
-    private void startMove(int screenX, int screenY) {
-        targetVector.set(screenX - spaceShipSprite.getHeight() / 2,
-                Gdx.graphics.getHeight() - screenY - spaceShipSprite.getHeight() / 2);
-        distanceVector.set(targetVector);
-        distanceVector.sub(positionSpaceShipVector);
-        directionVector.set(distanceVector);
-        directionVector.setLength(SPEED);
     }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        startMove(screenX, screenY);
+        logoSprite.startMove(new Vector2(screenX,
+                super.getScreenBounds().getHeight() - screenY).mul(super.getScreenToWorld()));
         return super.touchDown(screenX, screenY, pointer, button);
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
-        startMove(screenX, screenY);
+        logoSprite.startMove(new Vector2(screenX,
+                super.getScreenBounds().getHeight() - screenY).mul(super.getScreenToWorld()));
         return super.touchDragged(screenX, screenY, pointer);
     }
 
     @Override
-    public void resize(int width, int height) {
-        backgroundSprite.setSize(width, height);
-        super.resize(width, height);
+    public void resize(Rect worldBounds) {
+        backgroundSprite.resize(worldBounds);
+        logoSprite.resize(worldBounds);
     }
 }
