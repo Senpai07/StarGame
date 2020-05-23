15059472839a3d6a7d7f9a86bcdd9fd1eb65babd
Senpai <senpai@mail.ru>
2020-05-24 00:37:19 +0300

    Lesson 7 homework. Add button New Game and start game from begin.

--- core/src/ru/geekbrains/sprites/AlienShip.java
+++ core/src/ru/geekbrains/sprites/AlienShip.java
@@ -11,11 +11,10 @@ import ru.geekbrains.pools.ExplosionPool;
 
 public class AlienShip extends Ship {
 
-    private Vector2 fastSpeed;
+    private static final float V_Y = -0.3f;
 
     public AlienShip(BulletPool bulletPool, ExplosionPool explosionPool, Rect worldBounds, Sound sound) {
         super(bulletPool, explosionPool, worldBounds, sound);
-        fastSpeed = new Vector2(0, -0.5f);
     }
 
     public void set(TextureRegion[] regions,
@@ -29,7 +28,6 @@ public class AlienShip extends Ship {
                     float height) {
         this.regions = regions;
         this.moveVector.set(beginSpeed);
-        this.speedVector.set(beginSpeed);
         this.bulletRegion = bulletRegion;
         this.bulletHeight = bulletHeight;
         this.bulletSpeed.set(0, bulletVY);
@@ -38,19 +36,28 @@ public class AlienShip extends Ship {
         this.reloadTimer = reloadInterval;
         this.hp = hp;
         setHeightProportion(height);
+        speedVector.set(0, V_Y);
     }
 
     @Override
     public void update(float delta) {
         super.update(delta);
-        if (getTop() > worldBounds.getTop()) {
-            pos.mulAdd(fastSpeed, delta);
-        } else {
-            pos.mulAdd(speedVector, delta);
+        pos.mulAdd(speedVector, delta);
+        if (getTop() < worldBounds.getTop()) {
+            speedVector.set(moveVector);
+            bulletPosition.set(pos.x, pos.y - getHalfHeight());
+            autoShoot(delta);
         }
         if (getBottom() <= worldBounds.getBottom()) {
             destroy();
         }
     }
 
+    public boolean isBulletCollision(Bullet bullet) {
+        return !(bullet.getRight() < getLeft()
+                || bullet.getLeft() > getRight()
+                || bullet.getBottom() > getTop()
+                || bullet.getTop() < pos.y
+        );
+    }
 }
